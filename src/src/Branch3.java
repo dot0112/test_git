e6f28b5798ba2c74b614a8a2f7ef773092adf6e0
dot0112 <dot0112dot0112@gmail.com>
2025-08-04 09:18:30 +0900

    first commit for branch 3

---
+++ src/src/Branch3.java
@@ -0,0 +1,5 @@
+public class Branch3 {
+    public static void main(String[] args) {
+        System.out.println("Main - Branch 3");
+    }
+}
--- src/src/Main.java
+++ src/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("This is a test of how Git works");
         System.out.println("--- After create branch 1 ---");
         System.out.println("--- After create branch 2 ---");
+        System.out.println("--- Create branch 3 ---");
     }
 }
