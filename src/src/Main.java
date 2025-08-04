9c1275ac4cce53d3083025aee916b04f8fcfa327
dot0112 <dot0112dot0112@gmail.com>
2025-08-04 09:15:27 +0900

    after create branch 2

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("main branch");
         System.out.println("This is a test of how Git works");
         System.out.println("--- After create branch 1 ---");
+        System.out.println("--- After create branch 2 ---");
     }
 }
